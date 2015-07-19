364c8c0d1a9f9dff030d7d382cbe903bbb280b6c
tianbo <wangyitianfei@163.com>
2015-07-19 17:55:02 +0800

    【原因】下拉框联动自动取值

--- iproject/issue/views.py
+++ iproject/issue/views.py
@@ -1,18 +1,18 @@
-
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from issue.models import Task
 import json
 
 
-
-
 def home(req):
     return render_to_response('home.html')
+
+
 def issue(req):
     task = Task.objects.all()
     creators = Task.objects.raw('select id ,creator from issue_task')
-    return render_to_response('issue.html',{'task': task, 'creators': creators})
+    return render_to_response('issue.html', {'task': task, 'creators': creators})
+
 
 def settings(req):
     return render_to_response('settings.html')
@@ -28,5 +28,7 @@ def dummy(req):
 
 
 def getselect(req):
-    result = 'a'
-    return HttpResponse(json.dumps(result))
+    select = req.REQUEST.get('creator')
+
+    result = Task.objects.filter(creator=select).values('owner','task')[0]
+    return HttpResponse(json.dumps(result['owner']))
